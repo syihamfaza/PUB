fce5b67f066c7616536951b56a3f6ce35b59de0f
faza <faza453@gmail.com>
2024-03-12 13:20:43 +0700

    add API

--- projectUA_223210071/project_u_a_223210071/lib/app/modules/home/views/home_view.dart
+++ projectUA_223210071/project_u_a_223210071/lib/app/modules/home/views/home_view.dart
@@ -1,8 +1,12 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
-
+import 'dart:convert';
 import 'package:get/get.dart';
-
+import 'package:http/http.dart' as http;
 import '../controllers/home_controller.dart';
+import 'package:dropdown_search/dropdown_search.dart';
+import '../../../data/models/province_model.dart';
 
 class HomeView extends GetView<HomeController> {
   const HomeView({Key? key}) : super(key: key);
@@ -10,14 +14,29 @@ class HomeView extends GetView<HomeController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('HomeView'),
+        title: const Text('ONGKOS KIRIM'),
         centerTitle: true,
       ),
-      body: const Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
-        ),
+      body: ListView(
+        padding: EdgeInsets.all(20),
+        children: [
+          DropdownSearch<Province>(
+              label: "Provinsi",
+              onFind: (String filter) async {
+                Uri url =
+                    Uri.parse("https://api.rajaongkir.com/starter/province");
+                    try {
+                      final Response = await http.get(
+                        url,
+                        headers: {
+                          "key" : "1e05552c3337d4d2dcd7e6bd3e0456cb",
+                        },
+                      );
+                      var data = jsonDecode(Response.body) as Map<String, dynamic>;
+                      
+                    }
+              })
+        ],
       ),
     );
   }
